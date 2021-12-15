10a08a657fe6505930cbbad2c85af5ca2485184b
Peter Zheng <peter.zheng88228@gmail.com>
2021-12-14 20:54:22 -0500

    BCTC-99999/testagain

---
+++ src/components/PanelCard/index.tsx
@@ -0,0 +1,54 @@
+import classNames from 'classnames';
+import React, { ReactNode } from 'react';
+
+export interface PanelCardProps {
+  headerText: string;
+  children: ReactNode;
+  headerSubtitle?: string;
+  divider?: boolean;
+  footer?: ReactNode;
+  headerRight?: ReactNode;
+  padding?: boolean;
+}
+
+export const PanelCard: React.FC<PanelCardProps> = ({
+  headerText,
+  headerSubtitle,
+  children,
+  divider,
+  footer,
+  padding,
+  headerRight,
+}) => {
+  return (
+    <div
+      className={classNames('bg-white overflow-hidden shadow rounded-lg', {
+        'divide-y divide-gray-200': !!divider,
+      })}
+    >
+      <div className="px-4 py-5 sm:px-6 flex items-center justify-between flex-wrap sm:flex-nowrap">
+        <div>
+          {headerText && (
+            <h3 className="text-lg leading-6 font-medium text-gray-900">
+              {headerText}
+            </h3>
+          )}
+          {headerSubtitle && (
+            <p className="mt-1 max-w-2xl text-sm text-gray-500">
+              {headerSubtitle}
+            </p>
+          )}
+        </div>
+        {headerRight && headerRight}
+      </div>
+      <div
+        className={classNames('', {
+          'sm:p-6 px-4 py-5': padding,
+        })}
+      >
+        {children}
+      </div>
+      {footer && footer}
+    </div>
+  );
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,2 +1,3 @@
 export * from './components/UserInfoPill';
 export * from './components/ToggleSwitch';
+export * from './components/PanelCard';
