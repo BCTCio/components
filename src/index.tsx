ec95b4c15289d20ccba2921bc86ee18d9a6bff3d
Peter Zheng <peter.zheng88228@gmail.com>
2022-01-08 03:43:09 -0500

    exporting modal

--- src/index.tsx
+++ src/index.tsx
@@ -4,3 +4,4 @@ export * from './components/PanelCard';
 export * from './components/ConfirmationBox';
 export * from './components/Spinner';
 export * from './components/ButtonGroup';
+export * from './components/Modal';
