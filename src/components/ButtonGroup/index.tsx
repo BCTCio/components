8990f6d6566e28bba81c0af97f9695584d5215e7
Richard Xiong <richardx366@gmail.com>
2022-01-12 17:32:31 -0500

    requested touchups

--- src/components/ButtonGroup/index.tsx
+++ src/components/ButtonGroup/index.tsx
@@ -27,7 +27,7 @@ export const ButtonGroup: React.FC<ButtonGroupProps> = ({
   return (
     <button
       onClick={() => onClick?.call(null)}
-      className={`transition-colors relative border-2 border-gray-300 inline-flex items-center px-4 py-2 text-sm font-medium text-gray-700 focus:z-10 focus:outline-none focus:ring-1 focus:ring-green-500 ${roundLocation} ${
+      className={`transition-colors relative border border-gray-300 inline-flex items-center px-4 py-2 text-sm font-medium text-gray-700 focus:z-10 focus:outline-none focus:ring-1 focus:ring-green-500 ${roundLocation} ${
         selected
           ? 'cursor-default border-green-500 bg-white'
           : disabled
