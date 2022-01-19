9bda15915a30687c16f153d10e618f0afb1d64d0
Richard Xiong <richardx366@gmail.com>
2022-01-19 13:28:37 -0500

    confirm box css

--- src/components/ConfirmationBox/index.tsx
+++ src/components/ConfirmationBox/index.tsx
@@ -104,7 +104,7 @@ export const ConfirmationBox: React.FC<ConfirmationBoxProps> = ({
             leaveFrom="opacity-100 translate-y-0 sm:scale-100"
             leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
           >
-            <div className="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full sm:p-6">
+            <div className="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full sm:p-4 sm:pt-5">
               <div className="sm:flex sm:items-start">
                 <div
                   className={`${
