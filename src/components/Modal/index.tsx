759b462110b09e05d376520ef5483debf63d0207
Richard Xiong <richardx366@gmail.com>
2022-01-19 13:22:10 -0500

    modal css

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -64,12 +64,12 @@ export const Modal: React.FC<ModalProps> = ({
             leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
           >
             <div className="inline-block overflow-hidden text-left align-bottom transition-all transform bg-white rounded-lg shadow-xl sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
-              <div className="px-4 pt-5 pb-4 bg-white sm:p-6 sm:pb-4">
+              <div className="px-4 pt-5 pb-4 bg-white sm:p-5 sm:pb-4">
                 <div className="sm:flex sm:items-start">
-                  <div className="w-full mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                  <div className="w-full mt-3 text-center sm:mt-0 sm:ml-2 sm:text-left">
                     <Dialog.Title
                       as="h3"
-                      className="text-lg font-medium leading-6 text-gray-900"
+                      className="text-xl font-medium leading-6 text-gray-900"
                     >
                       {title}
                     </Dialog.Title>
@@ -78,17 +78,17 @@ export const Modal: React.FC<ModalProps> = ({
                 </div>
               </div>
 
-              <div className="px-4 py-3 bg-gray-50 sm:px-6 sm:flex sm:flex-row-reverse">
+              <div className="px-4 py-3 bg-gray-50 sm:flex sm:flex-row-reverse sm:text-sm">
                 <button
                   type="button"
-                  className="inline-flex justify-center w-full px-4 py-2 text-base font-medium text-white bg-green-600 border border-transparent rounded-md shadow-sm hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 sm:ml-3 sm:w-auto sm:text-sm"
+                  className="inline-flex justify-center w-full px-4 py-2 font-medium text-white bg-green-600 border border-transparent rounded-md shadow-sm hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 sm:ml-3 sm:w-auto"
                   onClick={onSubmit}
                 >
                   {submitText}
                 </button>
                 <button
                   type="button"
-                  className="inline-flex justify-center w-full px-4 py-2 mt-3 text-base font-medium text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+                  className="inline-flex justify-center w-full px-4 py-2 mt-3 font-medium text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:mt-0 sm:ml-3 sm:w-auto"
                   onClick={() => {
                     setShow(false);
                     onCancel?.call(null);
