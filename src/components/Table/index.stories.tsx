130b6dba94c20ddb525e0f3444d8edba80bcc598
Richard Xiong <richardx366@gmail.com>
2022-01-19 17:56:46 -0500

    table/pagination-changes

--- src/components/Table/index.stories.tsx
+++ src/components/Table/index.stories.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { Table } from './';
 
@@ -10,18 +10,46 @@ export default {
 
 const props = {
   columns: ['email', 'name', 'delete'],
-  data: new Array(20).fill(0).map((_, i) => ({id: i, email: `${i}@gmail.com`, name: `Name ${i}`, delete: <button>Delete</button>}))
+  data: new Array(20).fill(0).map((_, i) => ({
+    id: i,
+    email: `email${i + 1}@gmail.com`,
+    name: `Name ${i + 1}`,
+    delete: (
+      <button className="text-red-600 hover:text-red-500 font-medium">
+        Delete
+      </button>
+    ),
+  })),
 };
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 export const Default: ComponentStory<typeof Table> = (args) => {
-  return (
-    <Table {...args} {...props} />
-  );
+  return <Table {...args} {...props} />;
 };
 
 export const WithStripes: ComponentStory<typeof Table> = (args) => {
+  return <Table stripes {...args} {...props} />;
+};
+
+export const NoData: ComponentStory<typeof Table> = (args) => {
+  return <Table {...args} columns={props.columns} data={[]} name="users" />;
+};
+
+export const WithPagination: ComponentStory<typeof Table> = (args) => {
+  const [page, setPage] = useState(1);
   return (
-    <Table stripes {...args} {...props}/>
+    <>
+      <Table
+        {...args}
+        columns={props.columns}
+        data={props.data.slice((page - 1) * 5, page * 5)}
+        pagination={{
+          itemsPerPage: 5,
+          page,
+          setPage,
+          total: props.data.length,
+        }}
+      />
+    </>
   );
 };
