f65a695d9f92a30a7311909e872980b5d17d73b2
Richard Xiong <richardx366@gmail.com>
2022-01-20 22:40:12 -0500

    finish input

--- src/components/Input/index.stories.tsx
+++ src/components/Input/index.stories.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
-import { Input, InputProps } from './';
+import { Input } from './';
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
@@ -10,20 +10,71 @@ export default {
 
 const props = {
   label: 'Input',
-  description: 'Do stuff with this',
   placeholder: 'You put stuff here',
+  required: true,
 };
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-export const Default: ComponentStory<typeof Input> = (args) => {
+export const Default: ComponentStory<typeof Input> = (args: any) => {
+  const [state, setState] = useState('');
+  return <Input type="text" {...args} value={state} onChange={setState} />;
+};
+
+export const WithDetails: ComponentStory<typeof Input> = (args: any) => {
+  const [state, setState] = useState('');
+  return (
+    <Input
+      {...props}
+      type="text"
+      description="Do stuff with this"
+      {...args}
+      value={state}
+      onChange={setState}
+    />
+  );
+};
+
+export const WithError: ComponentStory<typeof Input> = (args: any) => {
+  const [state, setState] = useState('');
+  return (
+    <Input
+      {...props}
+      type="text"
+      error="Your input is terrible"
+      {...args}
+      value={state}
+      onChange={setState}
+    />
+  );
+};
+
+export const NumberWithLimits: ComponentStory<typeof Input> = (args: any) => {
   const [state, setState] = useState(0);
   return (
     <Input
       {...props}
+      type="number"
+      min={-50}
+      max={50}
+      description="Min is -50 and max is 50"
       {...args}
-      required
-      min={-5}
-      max={5}
+      value={state}
+      onChange={setState}
+    />
+  );
+};
+
+export const PasswordWithMaxLength: ComponentStory<typeof Input> = (
+  args: any
+) => {
+  const [state, setState] = useState('');
+  return (
+    <Input
+      {...props}
+      maxLength={30}
+      type="password"
+      description="Max length is 30 characters"
+      {...args}
       value={state}
       onChange={setState}
     />
--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,5 +1,10 @@
 import { Transition } from '@headlessui/react';
 import { InformationCircleIcon } from '@heroicons/react/outline';
+import {
+  ExclamationCircleIcon,
+  EyeIcon,
+  EyeOffIcon,
+} from '@heroicons/react/solid';
 import classNames from 'classnames';
 import React, { useEffect, useState, ChangeEventHandler } from 'react';
 
@@ -15,6 +20,7 @@ export interface InputProps {
   min?: number;
   max?: number;
   focusColor?: string;
+  error?: string;
 }
 
 export const Input: React.FC<InputProps> = ({
@@ -29,34 +35,48 @@ export const Input: React.FC<InputProps> = ({
   min,
   max,
   focusColor = 'focus:ring-blue-300',
+  error,
 }) => {
-  const [tooltip, setToolTip] = useState('');
+  const [tooltip, setTooltip] = useState('');
+  const [tooltipShow, setTooltipShow] = useState(false);
   const [passwordVis, setPasswordVis] = useState(false);
+
   useEffect(() => {
-    if (tooltip) {
+    if (tooltipShow) {
       const timeout = setTimeout(() => {
-        setToolTip('');
+        setTooltipShow(false);
       }, 2500);
       return () => clearTimeout(timeout);
     }
     return;
-  }, [tooltip]);
+  }, [tooltipShow]);
 
   const handleChange: ChangeEventHandler<HTMLInputElement> = (e) => {
-    const val = e.target.value;
-    if (
-      type === 'number' &&
-      (isNaN(+val) || (min && +val < min) || (max && +val > max))
-    )
-      return;
-    else if (maxLength && val.length > maxLength) return;
-    onChange(type === 'number' ? +val : val);
+    e.preventDefault();
+    let val = e.target.value;
+    if (type === 'number') {
+      if (min && +val < min) {
+        setTooltip(`Your number must be ${min} or above`);
+        setTooltipShow(true);
+        return;
+      }
+      if (max && +val > max) {
+        setTooltip(`Your number must be ${max} or under`);
+        setTooltipShow(true);
+        return;
+      }
+    } else {
+      if (maxLength && val.length > maxLength) {
+        setTooltip(`Your text can be at most ${maxLength} characters long`);
+        setTooltipShow(true);
+        return;
+      }
+    }
+    onChange((type === 'number' ? +val : val) as never);
   };
 
   const handleType = (): string => {
-    if (type === 'password') {
-      return passwordVis ? 'text' : 'password';
-    }
+    if (type === 'password' && passwordVis) return 'text';
     return type;
   };
 
@@ -69,43 +89,74 @@ export const Input: React.FC<InputProps> = ({
         {label}
         <span className="text-red-500">{required && ' *'}</span>
       </label>
-      <div className="mt-1">
+      <div className="mt-1 relative shadow-sm">
         <input
           name={label}
           id={label}
           type={handleType()}
           className={classNames(
-            focusColor,
-            'shadow-sm block w-full sm:text-sm border-gray-300 rounded-md'
+            {
+              'pr-16': type === 'password' && error,
+              'pr-10': +(type === 'password') ^ +!!error,
+            },
+            error
+              ? 'border-red-300 text-red-900 placeholder-red-300 focus:ring-red-500 focus:border-red-500'
+              : focusColor,
+            'shadow-sm block w-full sm:text-sm border-gray-300 rounded-md focus:outline-none'
           )}
           placeholder={placeholder}
           onChange={handleChange}
           value={value}
-          maxLength={maxLength}
-          max={max}
-          min={min}
         />
+        <div className="right-0 absolute inset-y-0 pr-3 flex items-center">
+          {type === 'password' &&
+            (passwordVis ? (
+              <EyeOffIcon
+                className="h-5 w-5 text-gray-500 hover:text-gray-400 cursor-pointer"
+                onClick={() => setPasswordVis(false)}
+              />
+            ) : (
+              <EyeIcon
+                className="h-5 w-5 text-gray-500 hover:text-gray-400 cursor-pointer"
+                onClick={() => setPasswordVis(true)}
+              />
+            ))}
+          {error && (
+            <ExclamationCircleIcon
+              className="ml-1 h-5 w-5 text-red-500"
+              aria-hidden="true"
+            />
+          )}
+        </div>
       </div>
-      <p className="mt-2 text-sm text-gray-500">{description}</p>
       <Transition
-        show={!!tooltip}
+        show={tooltipShow}
         leave="transition ease-out duration-200"
         enter="transition-opacity duration-200"
         enterFrom="opacity-0"
         enterTo="opacity-100"
         leaveTo="opacity-0 ease-in"
       >
-        <div className="absolute right-8 top-11 w-16 overflow-hidden inline-block">
-          <div className=" h-11 w-11 bg-gray-200 rotate-45 transform origin-bottom-left" />
-        </div>
-        <div className="absolute right-8 bg-gray-200 p-3 rounded-lg lg:w-4/12 sm:w-6/12 w-7/12 flex justify-between">
-          <InformationCircleIcon
-            className="h-6 w-12 text-gray-400 mr-2"
-            aria-hidden="true"
-          />
-          <span className="text-sm text-gray-600">{tooltip}</span>
+        <div className="flex flex-col items-end -mb-10">
+          <svg
+            className="fill-gray-200 h-2 mr-2"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 18"
+          >
+            <polygon points="12,0 0,18 24,18" />
+          </svg>
+          <div className="flex justify-between items-center bg-gray-200 p-2 rounded-lg">
+            <InformationCircleIcon
+              className="h-4 text-gray-400 mr-1"
+              aria-hidden="true"
+            />
+            <span className="text-xs text-gray-600">{tooltip}</span>
+          </div>
         </div>
       </Transition>
+      <p className="mt-2 text-red-600 text-sm">{error}</p>
+      <p className="mt-2 text-gray-500 text-sm">{description}</p>
     </div>
   );
 };
