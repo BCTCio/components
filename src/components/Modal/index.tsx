98e3105718722205226e5965d71085c0cfadc799
Richard Xiong <richardx366@gmail.com>
2022-01-21 20:33:19 -0500

    remove overflow-hidden for modal

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -63,8 +63,8 @@ export const Modal: React.FC<ModalProps> = ({
             leaveFrom="opacity-100 translate-y-0 sm:scale-100"
             leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
           >
-            <div className="inline-block overflow-hidden text-left align-bottom transition-all transform bg-white rounded-lg shadow-xl sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
-              <div className="px-4 pt-5 pb-4 bg-white sm:p-5 sm:pb-4">
+            <div className="inline-block text-left align-bottom transition-all transform shadow-xl sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+              <div className="px-4 pt-5 pb-4 bg-white sm:p-5 sm:pb-4 rounded-t-lg">
                 <div className="sm:flex sm:items-start">
                   <div className="w-full mt-3 text-center sm:mt-0 sm:ml-2 sm:text-left">
                     <Dialog.Title
@@ -78,7 +78,7 @@ export const Modal: React.FC<ModalProps> = ({
                 </div>
               </div>
 
-              <div className="px-4 py-3 bg-gray-50 sm:flex sm:flex-row-reverse sm:text-sm">
+              <div className="px-4 py-3 bg-gray-50 sm:flex sm:flex-row-reverse sm:text-sm rounded-b-lg">
                 <button
                   type="button"
                   className="inline-flex justify-center w-full px-4 py-2 font-medium text-white bg-green-600 border border-transparent rounded-md shadow-sm hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 sm:ml-3 sm:w-auto"
