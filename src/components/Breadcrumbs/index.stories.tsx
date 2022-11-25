733d24391002af72342bcf89129da59ef6b56b54
alex yang <alex@alexs-MacBook-Pro.local>
2022-11-25 16:38:34 -0500

    background color nav action added

--- src/components/Breadcrumbs/index.stories.tsx
+++ src/components/Breadcrumbs/index.stories.tsx
@@ -1,27 +1,27 @@
-import React from 'react';
-import { ComponentStory, ComponentMeta } from '@storybook/react';
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
 
-import { Breadcrumbs, BreadcrumbsProps } from './';
+import { Breadcrumbs, BreadcrumbsProps } from "./";
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
-  title: 'Elements/Breadcrumbs',
+  title: "Elements/Breadcrumbs",
   component: Breadcrumbs,
 } as ComponentMeta<typeof Breadcrumbs>;
 
 const props: BreadcrumbsProps = {
   links: [
     {
-      title: 'Title',
-      href: '/stories',
+      title: "Title",
+      href: "/stories",
     },
     {
-      title: 'Title',
-      href: '/stories',
+      title: "Title",
+      href: "/stories",
     },
     {
-      title: 'Title',
-      href: '/stories',
+      title: "Title",
+      href: "/stories",
       isCurrent: true,
     },
   ],
@@ -29,7 +29,5 @@ const props: BreadcrumbsProps = {
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 export const Default: ComponentStory<typeof Breadcrumbs> = (args) => (
-  <div className='px-2 py-2 border rounded-lg hover:border-gray-600'>
-    <Breadcrumbs {...props} {...args} />
-  </div>
+  <Breadcrumbs {...props} {...args} />
 );
--- src/components/Breadcrumbs/index.tsx
+++ src/components/Breadcrumbs/index.tsx
@@ -1,7 +1,7 @@
-import { ChevronRightIcon, HomeIcon } from '@heroicons/react/24/solid';
-import classNames from 'classnames';
+import { ChevronRightIcon, HomeIcon } from "@heroicons/react/24/solid";
+import classNames from "classnames";
 
-import React from 'react';
+import React from "react";
 
 export interface ILink {
   title: string;
@@ -14,61 +14,120 @@ export interface BreadcrumbsProps {
     component: any;
     hrefProperty: string;
   };
+  backgroundColor?: "light" | "dark";
   links: ILink[];
 }
 
+let background = "light";
+const DarkMode =
+  "bg-gray-900  flex px-2 py-2 border rounded-lg  hover:border-gray-600";
 export const Breadcrumbs: React.FC<BreadcrumbsProps> = ({
   links,
   customLink = {
     component: (props: any) => <a {...props} />,
-    hrefProperty: 'href',
+    hrefProperty: "href",
   },
+  backgroundColor = background,
 }) => {
-  return (
-    <nav className='flex' aria-label='Breadcrumb'>
-      <ol role='list' className='flex items-center space-x-4'>
-        <li>
-          <div>
-            <customLink.component
-              {...{ [customLink.hrefProperty]: links[0].href }}
-              className={classNames({
-                'text-gray-300': links.length > 1,
-                'hover:text-black': links.length > 1,
-                'hover:text-gray-200': links.length === 1,
-                'text-white': links.length === 1,
-              })}
-            >
-              <HomeIcon className='flex-shrink-0 w-5 h-5' aria-hidden='true' />
-              <span className='sr-only'>Home</span>
-            </customLink.component>
-          </div>
-        </li>
-        {links.slice(1).map((page, i) => (
-          <li key={i} title={page.title}>
-            <div className='flex items-center'>
-              <ChevronRightIcon
-                className='flex-shrink-0 w-5 h-5 text-gray-300'
-                aria-hidden='true'
-              />
+  if (backgroundColor === "light") {
+    return (
+      <nav
+        className="flex px-2 py-2 border border-gray-400 rounded-lg  hover:border-gray-600"
+        aria-label="Breadcrumb"
+      >
+        <ol role="list" className="flex items-center space-x-4">
+          <li>
+            <div>
               <customLink.component
-                {...{ [customLink.hrefProperty]: page.href }}
-                className={classNames(
-                  'max-w-xs truncate ml-4 text-sm font-medium',
-                  {
-                    'text-gray-300': !page.isCurrent,
-                    'hover:text-black': !page.isCurrent,
-                    'hover:text-gray-200': page.isCurrent,
-                    'text-white': page.isCurrent,
-                  },
-                )}
-                aria-current={page.isCurrent ? 'page' : undefined}
+                {...{ [customLink.hrefProperty]: links[0].href }}
+                className={classNames({
+                  "text-gray-400 hover:text-black": links.length > 1,
+
+                  "text-white hover:text-gray-200": links.length === 1,
+                })}
               >
-                {page.title}
+                <HomeIcon
+                  className="flex-shrink-0 w-5 h-5"
+                  aria-hidden="true"
+                />
+                <span className="sr-only">Home</span>
               </customLink.component>
             </div>
           </li>
-        ))}
-      </ol>
-    </nav>
-  );
+          {links.slice(1).map((page, i) => (
+            <li key={i} title={page.title}>
+              <div className="flex items-center">
+                <ChevronRightIcon
+                  className="flex-shrink-0 w-5 h-5 text-gray-400"
+                  aria-hidden="true"
+                />
+                <customLink.component
+                  {...{ [customLink.hrefProperty]: page.href }}
+                  className={classNames(
+                    "max-w-xs truncate ml-4 text-sm font-medium",
+                    {
+                      "text-gray-400 hover:text-black": !page.isCurrent,
+                      "text-gray-400 hover:text-gray-300": page.isCurrent,
+                    }
+                  )}
+                  aria-current={page.isCurrent ? "page" : undefined}
+                >
+                  {page.title}
+                </customLink.component>
+              </div>
+            </li>
+          ))}
+        </ol>
+      </nav>
+    );
+  }
+  if (backgroundColor === "dark") {
+    return (
+      <nav className={DarkMode} aria-label="Breadcrumb">
+        <ol role="list" className="flex items-center space-x-4">
+          <li>
+            <div>
+              <customLink.component
+                {...{ [customLink.hrefProperty]: links[0].href }}
+                className={classNames({
+                  "text-white hover:text-gray-200": links.length > 1,
+                  "text-white hover:cursor-not-allowed": links.length === 1,
+                })}
+              >
+                <HomeIcon
+                  className="flex-shrink-0 w-5 h-5"
+                  aria-hidden="true"
+                />
+                <span className="sr-only">Home</span>
+              </customLink.component>
+            </div>
+          </li>
+          {links.slice(1).map((page, i) => (
+            <li key={i} title={page.title}>
+              <div className="flex items-center">
+                <ChevronRightIcon
+                  className="flex-shrink-0 w-5 h-5 text-white"
+                  aria-hidden="true"
+                />
+                <customLink.component
+                  {...{ [customLink.hrefProperty]: page.href }}
+                  className={classNames(
+                    "max-w-xs truncate ml-4 text-sm font-medium",
+                    {
+                      "text-white hover:text-gray-200": !page.isCurrent,
+                      "text-white hover:cursor-not-allowed": page.isCurrent,
+                    }
+                  )}
+                  aria-current={page.isCurrent ? "page" : undefined}
+                >
+                  {page.title}
+                </customLink.component>
+              </div>
+            </li>
+          ))}
+        </ol>
+      </nav>
+    );
+  }
+  return <div>ERROR</div>;
 };
