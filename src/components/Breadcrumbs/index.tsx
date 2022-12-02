c9a64d6260a5a2f5ee560bce46a6719d79cd9e34
alex yang <alex@alexs-MacBook-Pro.local>
2022-12-01 20:38:12 -0500

    single quotes

--- src/components/Breadcrumbs/index.tsx
+++ src/components/Breadcrumbs/index.tsx
@@ -1,7 +1,7 @@
-import { ChevronRightIcon, HomeIcon } from "@heroicons/react/24/solid";
-import classNames from "classnames";
+import { ChevronRightIcon, HomeIcon } from '@heroicons/react/24/solid';
+import classNames from 'classnames';
 
-import React from "react";
+import React from 'react';
 
 export interface ILink {
   title: string;
@@ -14,63 +14,63 @@ export interface BreadcrumbsProps {
     component: any;
     hrefProperty: string;
   };
-  backgroundColor?: "light" | "dark";
+  backgroundColor?: 'light' | 'dark';
   links: ILink[];
 }
 
-let background = "light";
+let background = 'light';
 const DarkMode =
-  "bg-gray-900  flex px-2 py-2 border rounded-lg  hover:border-gray-600";
+  'bg-gray-900  flex px-2 py-2 border rounded-lg  hover:border-gray-600';
 export const Breadcrumbs: React.FC<BreadcrumbsProps> = ({
   links,
   customLink = {
     component: (props: any) => <a {...props} />,
-    hrefProperty: "href",
+    hrefProperty: 'href',
   },
   backgroundColor = background,
 }) => {
-  if (backgroundColor === "light") {
+  if (backgroundColor === 'light') {
     return (
       <nav
-        className="flex px-2 py-2 border border-gray-400 rounded-lg  hover:border-gray-600"
-        aria-label="Breadcrumb"
+        className='flex px-2 py-2 border border-gray-400 rounded-lg  hover:border-gray-600'
+        aria-label='Breadcrumb'
       >
-        <ol role="list" className="flex items-center space-x-4">
+        <ol role='list' className='flex items-center space-x-4'>
           <li>
             <div>
               <customLink.component
                 {...{ [customLink.hrefProperty]: links[0].href }}
                 className={classNames({
-                  "text-gray-400 hover:text-black": links.length > 1,
+                  'text-gray-400 hover:text-black': links.length > 1,
 
-                  "text-white hover:text-gray-200": links.length === 1,
+                  'text-white hover:text-gray-200': links.length === 1,
                 })}
               >
                 <HomeIcon
-                  className="flex-shrink-0 w-5 h-5"
-                  aria-hidden="true"
+                  className='flex-shrink-0 w-5 h-5'
+                  aria-hidden='true'
                 />
-                <span className="sr-only">Home</span>
+                <span className='sr-only'>Home</span>
               </customLink.component>
             </div>
           </li>
           {links.slice(1).map((page, i) => (
             <li key={i} title={page.title}>
-              <div className="flex items-center">
+              <div className='flex items-center'>
                 <ChevronRightIcon
-                  className="flex-shrink-0 w-5 h-5 text-gray-400"
-                  aria-hidden="true"
+                  className='flex-shrink-0 w-5 h-5 text-gray-400'
+                  aria-hidden='true'
                 />
                 <customLink.component
                   {...{ [customLink.hrefProperty]: page.href }}
                   className={classNames(
-                    "max-w-xs truncate ml-4 text-sm font-medium",
+                    'max-w-xs truncate ml-4 text-sm font-medium',
                     {
-                      "text-gray-400 hover:text-black": !page.isCurrent,
-                      "text-gray-400 hover:text-gray-300": page.isCurrent,
+                      'text-gray-400 hover:text-black': !page.isCurrent,
+                      'text-gray-400 hover:text-gray-300': page.isCurrent,
                     }
                   )}
-                  aria-current={page.isCurrent ? "page" : undefined}
+                  aria-current={page.isCurrent ? 'page' : undefined}
                 >
                   {page.title}
                 </customLink.component>
@@ -81,44 +81,44 @@ export const Breadcrumbs: React.FC<BreadcrumbsProps> = ({
       </nav>
     );
   }
-  if (backgroundColor === "dark") {
+  if (backgroundColor === 'dark') {
     return (
-      <nav className={DarkMode} aria-label="Breadcrumb">
-        <ol role="list" className="flex items-center space-x-4">
+      <nav className={DarkMode} aria-label='Breadcrumb'>
+        <ol role='list' className='flex items-center space-x-4'>
           <li>
             <div>
               <customLink.component
                 {...{ [customLink.hrefProperty]: links[0].href }}
                 className={classNames({
-                  "text-white hover:text-gray-200": links.length > 1,
-                  "text-white hover:cursor-not-allowed": links.length === 1,
+                  'text-white hover:text-gray-200': links.length > 1,
+                  'text-white hover:cursor-not-allowed': links.length === 1,
                 })}
               >
                 <HomeIcon
-                  className="flex-shrink-0 w-5 h-5"
-                  aria-hidden="true"
+                  className='flex-shrink-0 w-5 h-5'
+                  aria-hidden='true'
                 />
-                <span className="sr-only">Home</span>
+                <span className='sr-only'>Home</span>
               </customLink.component>
             </div>
           </li>
           {links.slice(1).map((page, i) => (
             <li key={i} title={page.title}>
-              <div className="flex items-center">
+              <div className='flex items-center'>
                 <ChevronRightIcon
-                  className="flex-shrink-0 w-5 h-5 text-white"
-                  aria-hidden="true"
+                  className='flex-shrink-0 w-5 h-5 text-white'
+                  aria-hidden='true'
                 />
                 <customLink.component
                   {...{ [customLink.hrefProperty]: page.href }}
                   className={classNames(
-                    "max-w-xs truncate ml-4 text-sm font-medium",
+                    'max-w-xs truncate ml-4 text-sm font-medium',
                     {
-                      "text-white hover:text-gray-200": !page.isCurrent,
-                      "text-white hover:cursor-not-allowed": page.isCurrent,
+                      'text-white hover:text-gray-200': !page.isCurrent,
+                      'text-white hover:cursor-not-allowed': page.isCurrent,
                     }
                   )}
-                  aria-current={page.isCurrent ? "page" : undefined}
+                  aria-current={page.isCurrent ? 'page' : undefined}
                 >
                   {page.title}
                 </customLink.component>
