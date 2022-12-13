a94046601cf5b92eec3a84f58bf1c70480db1a0d
alex yang <alex@alexs-MacBook-Pro.local>
2022-12-13 18:28:54 -0500

    delete some changes

--- src/components/Breadcrumbs/index.stories.tsx
+++ src/components/Breadcrumbs/index.stories.tsx
@@ -27,15 +27,13 @@ const props: BreadcrumbsProps = {
   ],
 };
 let className = '';
-if (props.darkMode === false) {
-  className =
-    'px-2 py-2 border border-gray-300 rounded-lg hover:border-gray-600';
-}
 if (props.darkMode === true) {
   className =
     'bg-gray-900 px-2 py-2 border border-gray-300 hover:border-gray-600 rounded-lg ';
-}
-if (!props.darkMode) {
+} else if (props.darkMode === false) {
+  className =
+    'px-2 py-2 border border-gray-300 rounded-lg hover:border-gray-600';
+} else {
   className =
     'dark:bg-gray-900 px-2 py-2 border border-gray-300 dark:hover:border-gray-600 rounded-lg hover:border-gray-600';
 }
--- src/components/Breadcrumbs/index.tsx
+++ src/components/Breadcrumbs/index.tsx
@@ -24,153 +24,52 @@ export const Breadcrumbs: React.FC<BreadcrumbsProps> = ({
     component: (props: any) => <a {...props} />,
     hrefProperty: 'href',
   },
-  darkMode,
 }) => {
-  if (darkMode === true) {
-    return (
-      <nav className='flex' aria-label='Breadcrumb'>
-        <ol role='list' className='flex items-center space-x-4'>
-          <li>
-            <div>
+  return (
+    <nav className='flex' aria-label='Breadcrumb'>
+      <ol role='list' className='flex items-center space-x-4'>
+        <li>
+          <div>
+            <customLink.component
+              {...{ [customLink.hrefProperty]: links[0].href }}
+              className={classNames({
+                'dark:text-gray-300 text-gray-600 hover:text-gray-800 dark:hover:text-gray-100':
+                  links.length > 1,
+                'dark:hover:text-gray-200 hover:text-gray-900 text-gray-700 dark:text-gray-100':
+                  links.length === 1,
+              })}
+            >
+              <HomeIcon className='flex-shrink-0 w-5 h-5' aria-hidden='true' />
+              <span className='sr-only'>Home</span>
+            </customLink.component>
+          </div>
+        </li>
+        {links.slice(1).map((page, i) => (
+          <li key={i} title={page.title}>
+            <div className='flex items-center'>
+              <ChevronRightIcon
+                className='flex-shrink-0 w-5 h-5 text-gray-300'
+                aria-hidden='true'
+              />
               <customLink.component
-                {...{ [customLink.hrefProperty]: links[0].href }}
-                className={classNames({
-                  'text-gray-300 hover:text-gray-100': links.length > 1,
-                  'hover:text-gray-200  xt-gray-100': links.length === 1,
-                })}
+                {...{ [customLink.hrefProperty]: page.href }}
+                className={classNames(
+                  'max-w-xs truncate ml-4 text-sm font-medium',
+                  {
+                    'hover:text-gray-700 text-gray-500 dark:text-gray-200 dark:hover:text-white':
+                      !page.isCurrent,
+                    'hover:text-gray-600 text-gray-400 dark:text-gray-300 dark:hover:text-gray-100':
+                      page.isCurrent,
+                  },
+                )}
+                aria-current={page.isCurrent ? 'page' : undefined}
               >
-                <HomeIcon
-                  className='flex-shrink-0 w-5 h-5'
-                  aria-hidden='true'
-                />
-                <span className='sr-only'>Home</span>
+                {page.title}
               </customLink.component>
             </div>
           </li>
-          {links.slice(1).map((page, i) => (
-            <li key={i} title={page.title}>
-              <div className='flex items-center'>
-                <ChevronRightIcon
-                  className='flex-shrink-0 w-5 h-5 text-gray-300'
-                  aria-hidden='true'
-                />
-                <customLink.component
-                  {...{ [customLink.hrefProperty]: page.href }}
-                  className={classNames(
-                    'max-w-xs truncate ml-4 text-sm font-medium',
-                    {
-                      'text-gray-200 hover:text-white': !page.isCurrent,
-                      'text-gray-300 hover:text-gray-100': page.isCurrent,
-                    }
-                  )}
-                  aria-current={page.isCurrent ? 'page' : undefined}
-                >
-                  {page.title}
-                </customLink.component>
-              </div>
-            </li>
-          ))}
-        </ol>
-      </nav>
-    );
-  }
-  if (darkMode === false) {
-    return (
-      <nav className='flex' aria-label='Breadcrumb'>
-        <ol role='list' className='flex items-center space-x-4'>
-          <li>
-            <div>
-              <customLink.component
-                {...{ [customLink.hrefProperty]: links[0].href }}
-                className={classNames({
-                  ' text-gray-600 hover:text-gray-800': links.length > 1,
-                  ' hover:text-gray-900 text-gray-700': links.length === 1,
-                })}
-              >
-                <HomeIcon
-                  className='flex-shrink-0 w-5 h-5'
-                  aria-hidden='true'
-                />
-                <span className='sr-only'>Home</span>
-              </customLink.component>
-            </div>
-          </li>
-          {links.slice(1).map((page, i) => (
-            <li key={i} title={page.title}>
-              <div className='flex items-center'>
-                <ChevronRightIcon
-                  className='flex-shrink-0 w-5 h-5 text-gray-300'
-                  aria-hidden='true'
-                />
-                <customLink.component
-                  {...{ [customLink.hrefProperty]: page.href }}
-                  className={classNames(
-                    'max-w-xs truncate ml-4 text-sm font-medium',
-                    {
-                      'hover:text-gray-700 text-gray-500': !page.isCurrent,
-                      'hover:text-gray-600 text-gray-400 ': page.isCurrent,
-                    }
-                  )}
-                  aria-current={page.isCurrent ? 'page' : undefined}
-                >
-                  {page.title}
-                </customLink.component>
-              </div>
-            </li>
-          ))}
-        </ol>
-      </nav>
-    );
-  } else {
-    return (
-      <nav className='flex' aria-label='Breadcrumb'>
-        <ol role='list' className='flex items-center space-x-4'>
-          <li>
-            <div>
-              <customLink.component
-                {...{ [customLink.hrefProperty]: links[0].href }}
-                className={classNames({
-                  'dark:text-gray-300 text-gray-600 hover:text-gray-800 dark:hover:text-gray-100':
-                    links.length > 1,
-                  'dark:hover:text-gray-200 hover:text-gray-900 text-gray-700 dark:text-gray-100':
-                    links.length === 1,
-                })}
-              >
-                <HomeIcon
-                  className='flex-shrink-0 w-5 h-5'
-                  aria-hidden='true'
-                />
-                <span className='sr-only'>Home</span>
-              </customLink.component>
-            </div>
-          </li>
-          {links.slice(1).map((page, i) => (
-            <li key={i} title={page.title}>
-              <div className='flex items-center'>
-                <ChevronRightIcon
-                  className='flex-shrink-0 w-5 h-5 text-gray-300'
-                  aria-hidden='true'
-                />
-                <customLink.component
-                  {...{ [customLink.hrefProperty]: page.href }}
-                  className={classNames(
-                    'max-w-xs truncate ml-4 text-sm font-medium',
-                    {
-                      'hover:text-gray-700 text-gray-500 dark:text-gray-200 dark:hover:text-white':
-                        !page.isCurrent,
-                      'hover:text-gray-600 text-gray-400 dark:text-gray-300 dark:hover:text-gray-100':
-                        page.isCurrent,
-                    }
-                  )}
-                  aria-current={page.isCurrent ? 'page' : undefined}
-                >
-                  {page.title}
-                </customLink.component>
-              </div>
-            </li>
-          ))}
-        </ol>
-      </nav>
-    );
-  }
+        ))}
+      </ol>
+    </nav>
+  );
 };
