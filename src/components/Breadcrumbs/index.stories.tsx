416f4c18b2b287bb449a57e31ff14cef07fce03b
alexusernamebruh <85073684+alexusernamebruh@users.noreply.github.com>
2023-01-22 14:32:47 -0500

    final

--- src/components/Breadcrumbs/index.stories.tsx
+++ src/components/Breadcrumbs/index.stories.tsx
@@ -26,20 +26,8 @@ const props: BreadcrumbsProps = {
     },
   ],
 };
-let className = '';
-if (props.darkMode === true) {
-  className =
-    'bg-gray-900 px-2 py-2 border border-gray-300 hover:border-gray-600 rounded-lg ';
-} else if (props.darkMode === false) {
-  className =
-    'px-2 py-2 border border-gray-300 rounded-lg hover:border-gray-600';
-} else {
-  className =
-    'dark:bg-gray-900 px-2 py-2 border border-gray-300 dark:hover:border-gray-600 rounded-lg hover:border-gray-600';
-}
+
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 export const Default: ComponentStory<typeof Breadcrumbs> = (args) => (
-  <div className={className}>
-    <Breadcrumbs {...props} {...args} />
-  </div>
+  <Breadcrumbs {...props} {...args} />
 );
--- src/components/Breadcrumbs/index.tsx
+++ src/components/Breadcrumbs/index.tsx
@@ -24,20 +24,35 @@ export const Breadcrumbs: React.FC<BreadcrumbsProps> = ({
     component: (props: any) => <a {...props} />,
     hrefProperty: 'href',
   },
+  darkMode,
 }) => {
   return (
-    <nav className='flex' aria-label='Breadcrumb'>
+    <nav
+      className={`flex ${
+        darkMode
+          ? 'bg-gray-900 px-2 py-2 border border-gray-300 hover:border-gray-600 rounded-lg'
+          : 'px-2 py-2 border border-gray-300 rounded-lg hover:border-gray-600'
+      }`}
+      aria-label='Breadcrumb'
+    >
       <ol role='list' className='flex items-center space-x-4'>
         <li>
           <div>
             <customLink.component
               {...{ [customLink.hrefProperty]: links[0].href }}
-              className={classNames({
-                'dark:text-gray-300 text-gray-600 hover:text-gray-800 dark:hover:text-gray-100':
-                  links.length > 1,
-                'dark:hover:text-gray-200 hover:text-gray-900 text-gray-700 dark:text-gray-100':
-                  links.length === 1,
-              })}
+              className={`${
+                links.length > 1
+                  ? `${
+                      darkMode
+                        ? 'text-gray-300 hover:text-gray-100'
+                        : 'text-gray-600 hover:text-gray-800'
+                    }`
+                  : `${
+                      darkMode
+                        ? 'hover:text-gray-200 text-gray-100'
+                        : 'hover:text-gray-900 text-gray-700'
+                    }  `
+              }`}
             >
               <HomeIcon className='flex-shrink-0 w-5 h-5' aria-hidden='true' />
               <span className='sr-only'>Home</span>
@@ -53,15 +68,19 @@ export const Breadcrumbs: React.FC<BreadcrumbsProps> = ({
               />
               <customLink.component
                 {...{ [customLink.hrefProperty]: page.href }}
-                className={classNames(
-                  'max-w-xs truncate ml-4 text-sm font-medium',
-                  {
-                    'hover:text-gray-700 text-gray-500 dark:text-gray-200 dark:hover:text-white':
-                      !page.isCurrent,
-                    'hover:text-gray-600 text-gray-400 dark:text-gray-300 dark:hover:text-gray-100':
-                      page.isCurrent,
-                  },
-                )}
+                className={`max-w-xs truncate ml-4 text-sm font-medium ${
+                  page.isCurrent
+                    ? ` ${
+                        darkMode
+                          ? 'text-gray-300 hover:text-gray-100'
+                          : 'hover:text-gray-600 text-gray-400'
+                      }`
+                    : `${
+                        darkMode
+                          ? 'text-gray-200 hover:text-white'
+                          : 'hover:text-gray-700 text-gray-500'
+                      }`
+                }`}
                 aria-current={page.isCurrent ? 'page' : undefined}
               >
                 {page.title}
