493e785cd2e1cbe9d9c0b492f8d5a8e1dd12e520
alexusernamebruh <85073684+alexusernamebruh@users.noreply.github.com>
2023-02-05 11:52:43 -0500

    more fix

--- src/components/Breadcrumbs/index.tsx
+++ src/components/Breadcrumbs/index.tsx
@@ -37,12 +37,12 @@ export const Breadcrumbs: React.FC<BreadcrumbsProps> = ({
                   ? `${
                       darkMode
                         ? 'text-gray-300 hover:text-gray-100'
-                        : 'text-gray-600 hover:text-gray-800'
+                        : 'text-gray-400 hover:text-gray-600'
                     }`
                   : `${
                       darkMode
                         ? 'hover:text-gray-200 text-gray-100'
-                        : 'hover:text-gray-900 text-gray-700'
+                        : 'hover:text-gray-600 text-gray-400'
                     }  `
               }`}
             >
@@ -70,7 +70,7 @@ export const Breadcrumbs: React.FC<BreadcrumbsProps> = ({
                     : `${
                         darkMode
                           ? 'text-gray-200 hover:text-white'
-                          : 'hover:text-gray-700 text-gray-500'
+                          : 'hover:text-gray-600 text-gray-400'
                       }`
                 }`}
                 aria-current={page.isCurrent ? 'page' : undefined}
